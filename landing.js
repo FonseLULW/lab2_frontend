ef23d2dbc109e5a6df0c2f08645584e97af53278
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2023-01-17 08:28:55 -0800

    initial commit

---
+++ landing.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	$('#chatbot-form').submit(function(event) {
+		event.preventDefault();
+		const message = $('#chatbot-input').val();
+		$.ajax({
+			type: 'POST',
+			url: 'https://lab2-fpgd.onrender.com',
+			data: {
+				message: message
+			},
+			success: function(response) {
+                let newMessage = $('<div>', {
+                    class: 'message'
+                }).text(response.text);
+                let removeButton = $('<button>', {
+                    class: 'remove-button'
+                }).text('Remove');
+                newMessage.append(removeButton);
+                $('#chat-history').append(newMessage);
+            }
+		});
+	});
+});
+
+$(document).on('click', '.remove-button', function() {
+	$(this).parent().remove();
+});
